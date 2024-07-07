41b248fcc7fab649ba2488bc10118c89ac1e7eeb
Mike Kress <49699285+eDad2003@users.noreply.github.com>
2024-07-07 14:46:56 -0400

    Update index.php

--- homepage/index.php
+++ homepage/index.php
@@ -26,9 +26,8 @@ echo "<a href=\"https://github.com/eDad2003/BirdNET-Pi.git\" target=\"_blank\"><
 #echo "<a href=\"https://github.com/Nachtzuster/BirdNET-Pi.git\" target=\"_blank\"><img src=\"images/bird.png\"></a>";
 echo "<a href=\"https://github.com/eDad2003/BirdNET-Pi.git\" target=\"_blank\"><img src=\"images/bird.png\"></a>";
 }?>
-  <div>eDad2003 Dev Branch</div>
   </div>
-
+  <div>eDad2003 Dev Branch</div>
 
   <div class="stream">
 <?php
